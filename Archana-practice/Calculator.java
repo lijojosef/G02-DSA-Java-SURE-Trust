d54367c556722b7c1d22adb1da4e274828b48a32
archanachowla <carchana01012004@gmail.com>
2024-12-28 15:26:52 +0530

    calculator task added to the repository

---
+++ Archana-practice/Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number 1: ");
+        int num1=sc.nextInt();
+        System.out.println("enter number 1: ");
+        int num2=sc.nextInt();
+        System.out.println("enter the operator to perform operation : ");
+        char ch=sc.next().charAt(0);
+        switch(ch){
+            case '+':
+            System.out.println(num1+num2);
+            break;
+            case '-':
+            System.out.println(num1-num2);
+            break;
+            case '*':
+            System.out.println(num1*num2);
+            break;
+            case '/':
+            System.out.println(num1/num2);
+            break;
+            case '%':
+            System.out.println(num1%num2);
+            break;
+            default:
+            System.out.println("Ivalid expression");
+
+        }
+    }
+
+}
