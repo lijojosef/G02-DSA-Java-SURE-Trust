7bc6cb57651218315d0c414fac675ffeb55f30d8
Bhavani-Shankar13 <bhavanishankar131204@gmail.com>
2024-12-30 21:14:02 +0530

    Armstrong

---
+++ Bhavani-shankar-Practice/Armstrong.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Armstrong {
+    static boolean isarmstrong(int num){
+        int temp = num;
+        int answer = 0;
+        int nums = String.valueOf(num).length();
+
+        while (temp != 0) {
+            int result = temp % 10;
+            answer += Math.pow(result, nums);
+            temp /= 10;
+        }
+        return num == answer;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num =sc.nextInt();
+        boolean res = isarmstrong(num);
+        System.out.println(res);
+    }
+}
