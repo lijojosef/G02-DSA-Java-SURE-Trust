584e91627b917d7009e1ae77f384f0e69e1a5935
S.Sri Thrayokya <140336022+SriThraylokya@users.noreply.github.com>
2025-01-05 23:22:10 +0530

    Update ArmStrong.java

--- SriThraylokya_SRC/ArmStrong.java
+++ SriThraylokya_SRC/ArmStrong.java
@@ -8,17 +8,36 @@ public class ArmStrong {
         System.out.print("Enter a 3-digit number :  ");
         int n= sc.nextInt();
         //153-->1*3 + 5*3 +3*3 =153
-        int sum=0,rem=0;
-        while(n>0) {
-            rem = n % 10;
+        int sum=0,rem=0,num=n;
+        while(num>0) {
+            rem = num % 10;
             sum = sum + (rem * rem * rem);
-            n = n / 10;
+            num = num / 10;
         }
-        System.out.println(sum);
+        System.out.print("sum is : "+sum+" so, ");
         if(n == sum ){
-            System.out.println( n +" is an ArmStrong number ");
+            System.out.print( n +" is an ArmStrong number ");
+        }else {
+            System.out.print(n+" is not an Armstrong number ");
         }
 
-    }
+
+
+//        int number = 371, originalNumber, remainder, result = 0;
+//
+//        originalNumber = number;
+//
+//        while (originalNumber != 0)
+//        {
+//            remainder = originalNumber % 10;
+//            result += Math.pow(remainder, 3);
+//            originalNumber /= 10;
+//        }
+//
+//        if(result == number)
+//            System.out.println(number + " is an Armstrong number.");
+//        else
+//            System.out.println(number + " is not an Armstrong number.");
+   }
 
 }
