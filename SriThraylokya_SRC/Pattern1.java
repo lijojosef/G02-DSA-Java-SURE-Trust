cc58e5b09ad14a441b07cb35187f76e66f0803d7
S.Sri Thrayokya <140336022+SriThraylokya@users.noreply.github.com>
2025-01-06 22:50:49 +0530

    Update Pattern1.java

--- SriThraylokya_SRC/Pattern1.java
+++ SriThraylokya_SRC/Pattern1.java
@@ -1,15 +1,30 @@
 package SriThraylokya_SRC;
 
+
 public class Pattern1 {
     public static void main(String[] args) {
         System.out.println("..pattern");
-        int n=5;
+        int n=5,k=4;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i+1; j++) {
                     System.out.print("#");
             }
             System.out.println(" ");
         }
+        patter2(k);
+    }
+
+
+    public static void patter2(int k){
+        for (int i = 0; i < k; i++) {
+            for (int j = i; j < k+1; j++) {
+                System.out.print("$");
+            }
+            System.out.println(" ");
+
+        }
     }
 }
+
+
 
