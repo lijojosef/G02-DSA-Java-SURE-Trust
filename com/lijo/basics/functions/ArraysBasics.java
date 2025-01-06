7641032bb5156585bf7a0e9c930bba657a743e40
Lijo Joseph <lijojoseph234@gmail.com>
2025-01-06 06:50:48 +0530

    Day 8

---
+++ com/lijo/basics/functions/ArraysBasics.java
@@ -0,0 +1,61 @@
+package com.lijo.basics.functions;
+
+public class ArraysBasics {
+    public static void main(String[] args) {
+        // initialization :
+        // data_type[] name = new data_type[];
+//        int[] rno = new int[5];
+//
+//        rno[2] = 5;
+//
+//
+//        int[] rnos = {2, 34, 56, 8, 9};
+//
+//        // display:
+//        for (int i = 0; i < 5; i++){
+//            System.out.print(rnos[i]+" ");
+//        }
+//        System.out.println();
+//
+//        //enhanced for loops:
+//        for(int num: rnos){
+//            System.out.print(num + " ");
+//        }
+//
+//        System.out.println();
+//
+//
+//        //using Arrays library
+//        System.out.println(Arrays.toString(rnos));
+
+        //multidimensional array:
+//        int[][] arr = new int[3][];
+
+        //
+//        int[][] arr1 = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {8, 9, 10}
+//        };
+
+        //1)how to take input in an array
+        //2)how to take input and output in a multidimensional array from a user
+
+        int[] arr = {1, 3, 23, 9, 18};
+        swap(arr, 1, 3);
+        
+        
+        reverse(arr);
+    }
+
+    private static void reverse(int[] arr) {
+
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    
+}
