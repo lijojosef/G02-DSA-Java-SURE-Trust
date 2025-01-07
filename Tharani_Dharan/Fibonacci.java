3e15ae1546c3b9ef1f9b211f085a7d002b466388
TharaniDharan10 <TharaniDharan.S@outlook.com>
2025-01-07 17:19:59 +0530

    Updated Fibonicci

--- Tharani_Dharan/Fibonacci.java
+++ Tharani_Dharan/Fibonacci.java
@@ -5,16 +5,33 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");
+        System.out.print("Enter a positive number : ");
         int num = sc.nextInt();
-        int a = 0;
-        int b = 1;
+
+        int previous = 0;
+        int next = 1;
         int count = 2;
         int temp = 0;
+
+        if(num<0){
+            System.out.println("Enter a Positive Number");
+            return;
+        }
+
+        if(num==0){
+            System.out.println("Fibonacci value of number "+num+" is : "+previous);
+            return;
+        }
+
+        if(num==1){
+            System.out.println("Fibonacci value of number "+num+" is : "+next);
+            return;
+        }
+
         while(count<=num){
-            temp = a+b;
-            a = b;
-            b = temp;
+            temp = previous+next;
+            previous = next;
+            next = temp;
             count++;
         }
         System.out.println("Fibonacci value of number "+num+" is : "+temp);
